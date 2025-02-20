8f461ab84dcd2937f41470cc1f2233bbc492c99f
Leonamin <yism810@gmail.com>
2025-02-20 18:04:56 +0900

    # ADD - UI 조정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./styles.css";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div style={{ width: "100vw", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <h1>파일 업로드 & 다운로드</h1>
       <FileManager />
     </div>
